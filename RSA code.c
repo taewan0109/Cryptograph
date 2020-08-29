a75febfb3a92d4979637c7c9e6d964b95aff055f
Taewan Kim <42868790+taewan0109@users.noreply.github.com>
2020-08-29 16:03:09 +0900

    Update RSA code.c

--- RSA code.c
+++ RSA code.c
@@ -4,59 +4,86 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define MIN 400
-#define MAX 500
+#define MIN 900
+#define MAX 1000
+#define INLEN 20
 
-void makeKey(int*, int*, int*, int*, int*);
-int encryption(int, int, int);
-int decryption(int, int, int);
-int decryption_fast(int, int, int, int, int);
-void makePQ(int *, int *);
-int makeCoprime(int);
-int makeInverse(int, int);
-int returnGCD(int, int);
-bool isPrime(int);
+typedef unsigned long long DATA;
+typedef long double RDATA;
+
+void makeKey(DATA*, DATA*, DATA*, DATA*, DATA*);
+DATA encryption(DATA, DATA, DATA);
+DATA decryption(DATA, DATA, DATA);
+DATA decryption_fast(DATA, DATA, DATA, DATA, DATA);
+void makePQ(DATA*, DATA*);
+DATA makeCoprime(DATA);
+DATA makeInverse(DATA, DATA);
+DATA returnGCD(DATA, DATA);
+bool isPrime(DATA);
 
 int main(void)
 {
-    int N, e, d, p, q, plane, cipher, decrypted, decrypted_fast;
+    DATA N, e, d, p, q, plane, cipher, decrypted, decrypted_fast;
+    DATA plane_s[INLEN] = { 0, }, cipher_s[INLEN] = { 0, };
+    char str[INLEN] = { 0, }, decrypted_s[INLEN] = { 0, }, decrypted_fast_s[INLEN] = { 0, };
+    int i;
 
     srand((unsigned int)time(NULL));
     makeKey(&N, &e, &d, &p, &q);
 
-    plane = 100;
+    printf("암호화할 문자열을 입력해 주세요(%d자 이내): ", INLEN);
+    gets(str);
+
+    printf("\n평문: %s\n\n", str);
+
+    printf("10진수: ");
+    for (i = 0; i < INLEN; i++)
+    {
+        printf("%d ", str[i]);
+        plane_s[i] = (DATA)str[i];
+        cipher_s[i] = encryption(N, e, plane_s[i]);
+        decrypted_s[i] = (char)decryption(N, d, cipher_s[i]);
+        decrypted_fast_s[i] = (char)decryption_fast(N, d, cipher_s[i], p, q);
+    }
+    
+    printf("\n\n암호문: ");
+    for (i = 0; i < INLEN; i++)
+        printf("%lld ", cipher_s[i]);
+
+    printf("\n\n복호문: %s\n", decrypted_fast_s);
+    
+    /*plane = 100000;
     cipher = encryption(N, e, plane);
     decrypted = decryption(N, d, cipher);
     decrypted_fast = decryption_fast(N, d, cipher, p, q);
 
-    printf("plane text: %d\n", plane);
-    printf("cipher text: %d\n", cipher);
-    printf("decrypted text: %d\n", decrypted);
-    printf("decrypted text(fast): %d\n", decrypted_fast);
+    printf("plane text: %lld\n", plane);
+    printf("cipher text: %lld\n", cipher);
+    printf("decrypted text: %lld\n", decrypted);
+    printf("decrypted text(fast): %lld\n", decrypted_fast);*/
 
-	return 0;
+    return 0;
 }
 
-void makeKey(int* N, int* e, int* d, int* p, int* q)
+void makeKey(DATA* N, DATA* e, DATA* d, DATA* p, DATA* q)
 {
-    //int p, q, phi;
-    int phi;
+    DATA phi;
 
     makePQ(p, q);
     *N = (*p) * (*q);
     phi = (*p - 1) * (*q - 1);
-    printf("p: %d, q: %d, N: %d, phi: %d\n", *p, *q, *N, phi);
+    printf("p: %lld, q: %lld, N: %lld, phi: %lld\n", *p, *q, *N, phi);
 
     *e = makeCoprime(phi);
     *d = makeInverse(*e, phi);
-    printf("N: %d, e: %d, d: %d\n\n", *N, *e, *d);
+    printf("N: %lld, e: %lld, d: %lld\n\n", *N, *e, *d);
 }
 
-int encryption(int N, int e, int num)
+DATA encryption(DATA N, DATA e, DATA num)
 {
-    int prod = 1;
+    DATA prod = 1;
 
-    for (int i = 0; i < e; i++)
+    for (DATA i = 0; i < e; i++)
     {
         prod *= num % N;
         prod %= N;
@@ -64,11 +91,11 @@ int encryption(int N, int e, int num)
     return prod;
 }
 
-int decryption(int N, int d, int num)
+DATA decryption(DATA N, DATA d, DATA num)
 {
-    int prod = 1;
+    DATA prod = 1;
 
-    for (int i = 0; i < d; i++)
+    for (DATA i = 0; i < d; i++)
     {
         prod *= num % N;
         prod %= N;
@@ -77,53 +104,59 @@ int decryption(int N, int d, int num)
     return prod;
 }
 
-int decryption_fast(int N, int d, int num, int p, int q)
+DATA decryption_fast(DATA N, DATA d, DATA num, DATA p, DATA q)
 {
-    int d_p, d_q, q_inv;
-    int m_1 = 1, m_2 = 1, m_3, h, i;
+    DATA d_p, d_q, q_inv;
+    DATA m_1 = 1, m_2 = 1, m_3, h, i;
 
     d_p = d % (p - 1);
     d_q = d % (q - 1);
 
     for (i = 1; i < p; i++)
-        if ((q * i) % p == 1)   q_inv = i;
+        if ((q * i) % p == 1)
+        {
+            q_inv = i;
+            break;
+        }
+    if (q_inv != i) exit(0);
 
-    for (i = 0; i < d_p; i++){
+
+    for (i = 0; i < d_p; i++) {
         m_1 *= num % p;
         m_1 %= p;
     }
 
-    for (i = 0; i < d_q; i++){
+    for (i = 0; i < d_q; i++) {
         m_2 *= num % q;
         m_2 %= q;
     }
 
     m_3 = (m_1 - m_2);
-    if (m_3 < 0)
+    if (m_1 < m_2)
         for (i = 1; ; i++)
-            if (m_3 + i * p > 0)
+            if ((m_1 + i * p) +m_2 > 0)
             {
                 m_3 += i * p;
                 break;
             }
-    
+
     h = (q_inv * m_3) % p;
     return m_2 + h * q;
 }
 
-bool isPrime(int num)
+bool isPrime(DATA num)
 {
-    for (int i = 2; i <= sqrt((double)num); i++)
+    for (DATA i = 2; i <= sqrt((RDATA)num); i++)
         if (num % i == 0)
             return false;
 
     return true;
 }
 
-void makePQ(int * p, int * q)
+void makePQ(DATA* p, DATA* q)
 {
-    int i, len = 0, r_1, r_2;
-    int primeArr[(MAX - MIN) / 2] = { 0, };
+    DATA i, len = 0, r_1, r_2;
+    DATA primeArr[(MAX - MIN) / 2] = { 0, };
 
     for (i = MIN; i < MAX; i++)
         if (isPrime(i))
@@ -131,23 +164,23 @@ void makePQ(int * p, int * q)
             primeArr[len] = i;
             len++;
         }
-    
+
     r_1 = rand() % len;
     *p = primeArr[r_1];
-    while (1){
+    while (1) {
         r_2 = rand() % len;
-        if (r_1 != r_2){
+        if (r_1 != r_2) {
             *q = primeArr[r_2];
             break;
         }
     }
-        
+
 }
 
-int makeCoprime(int phi)
+DATA makeCoprime(DATA phi)
 {
-    int* copArr = (int *)malloc(phi * sizeof(int));
-    int len = 0, p;
+    DATA* copArr = (DATA*)malloc(phi * sizeof(DATA));
+    DATA len = 0, p;
 
     if (copArr == NULL)
     {
@@ -155,27 +188,27 @@ int makeCoprime(int phi)
         exit(0);
     }
 
-    for (int i = 2; i <= phi; i++)
+    for (DATA i = 2; i <= phi; i++)
         if (returnGCD(i, phi) == 1)
         {
             copArr[len - 1] = i;
             len++;
         }
 
-    p = (int)pow((double)phi, 1.0/4.0);
+    p = (DATA)pow((RDATA)phi, 1.0 / 4.0);
     return copArr[rand() % p];
 }
 
-int returnGCD(int p, int q)
+DATA returnGCD(DATA p, DATA q)
 {
     if (p == 0 || q == 0)   return p + q;
     else if (p >= q)    return returnGCD(q, p % q);
     else    return returnGCD(p, q % p);
 }
 
-int makeInverse(int e, int phi)
+DATA makeInverse(DATA e, DATA phi)
 {
-    for (int i = 1; i < phi; i++)
+    for (DATA i = 1; i < phi; i++)
         if ((e * i) % phi == 1)
             return i;
 
